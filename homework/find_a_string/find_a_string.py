385b7189117efda3707a117b9086213ff6ab9fea
b.bettsss <b.betttsss@gmail.com>
2024-10-19 18:40:22 +0000

    betts_hf4_1

--- homework/find_a_string/find_a_string.py
+++ homework/find_a_string/find_a_string.py
@@ -1,3 +1,8 @@
 def find_a_string(original_str: str, substr: str) -> int:
     count = original_str.count(substr)
     return count
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 52c6c02 (betts_hf4_1)
--- homework/swap_case/swap_case.py
+++ homework/swap_case/swap_case.py
@@ -1,4 +1,5 @@
 def swap_case(s: str) -> int:
+<<<<<<< HEAD
     string = ""
 
     for letters in s:
@@ -10,3 +11,19 @@ def swap_case(s: str) -> int:
             string += letters
 
     return string
+=======
+    new_string = ""
+
+    for a in s:
+        if a.isupper():
+            new_string += a.lower()
+            
+        elif a.islower():
+            new_string += a.upper()
+
+        else:
+            new_string += a
+
+    return new_string
+
+>>>>>>> 52c6c02 (betts_hf4_1)
