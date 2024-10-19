7fd96c30576df9174f59bc1b02a2e3f8b9eeb9cd
b.bettsss <b.betttsss@gmail.com>
2024-10-19 18:50:35 +0000

    betts_hf4_2

--- homework/minion_game/minion_game.py
+++ homework/minion_game/minion_game.py
@@ -1,4 +1,5 @@
 def minion_game(s: str) -> str:
+<<<<<<< HEAD
     kevin_points = 0
     stuart_points = 0
      
@@ -14,3 +15,24 @@ def minion_game(s: str) -> str:
         print('Kevin ' + str(kevin_points))
     else:
         print('Draw')
+=======
+    length = len(s)
+    vowel = 0
+    consonant = 0
+
+    for i in range(length):
+        if s[i] in 'AEIOU':
+                vowel += (length-i)
+        else:
+             consonant += (length-i)
+
+    if vowel > consonant:
+        return f"Kevin {vowel}"
+    elif consonant > vowel:
+        return f"Stuart {consonant}"
+    else:
+        return "Draw" 
+
+    return minion_game
+
+>>>>>>> c733f68 (betts_hf4_2)
